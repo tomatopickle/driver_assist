5ee5bfc63d6bbed775c821a80973a437ca6b3f84
Abaan Shanid <abaanashanid@gmail.com>
2022-11-06 15:10:06 +0300

    Feat: Basic functionality

---
+++ src/views/PredictionRenderer.js
@@ -0,0 +1,34 @@
+export default class PredictionRenderer {
+  render(predictions, ctx) {
+    //color
+    const color = "#41b883";
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = color;
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = color;
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  }
+}
--- src/views/TripView.vue
+++ src/views/TripView.vue
@@ -1,5 +1,98 @@
 <template>
   <div>
-    <h1>This is an about page</h1>
+    <div class="video-player">
+      <video ref="video" />
+      <canvas width="640" height="480" ref="canvas" />
+    </div>
   </div>
 </template>
+
+<script lang="js">
+import * as cocoSSD from "@tensorflow-models/coco-ssd";
+import PredictionRenderer from "./PredictionRenderer";
+
+var predictionRenderer = new PredictionRenderer();
+
+export default {
+    data: () => {
+        return {
+            predictionRenderer: predictionRenderer,
+            predictionInterval: Number,
+            model: cocoSSD.ObjectDetection,
+            loading: true
+        };
+    },
+    props: {
+    },
+    async created() {
+        this.model = await cocoSSD.load();
+        this.loading = false;
+    },
+    async mounted() {
+        const video = await this.initWebcam(this.$refs.video);
+        this.predictionInterval = setInterval(() => {
+            this.analyzeVideoFrame(video);
+        }, 50);
+    },
+
+    beforeDestroy() {
+        clearInterval(this.predictionInterval);
+    },
+    methods: {
+        initWebcam(videoRef) {
+
+            return new Promise(resolve => {
+
+                navigator.mediaDevices
+                    .getUserMedia({
+                        video: {
+                            width: 640,
+                            height: document.documentElement.clientHeight
+                        }
+                    })
+                    .then(stream => {
+
+                        const video = videoRef;
+                        this.$refs.canvas.height = document.documentElement.clientHeight
+                        video.srcObject = stream;
+                        video.onloadedmetadata = () => {
+                            video.play();
+                            resolve(video);
+                        }
+                    });
+            });
+        },
+
+        analyzeVideoFrame(video) {
+
+            this.model.detect(video).then((predictions) => {
+
+                const canvas = this.$refs.canvas;
+                const ctx = canvas.getContext("2d");
+
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                this.predictionRenderer.render(predictions, ctx);
+            });
+        }
+    },
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.video-player {
+  position: relative;
+
+  canvas,
+  video {
+    border-radius: 5px;
+  }
+
+  canvas {
+    position: absolute;
+    margin-left: -640px;
+  }
+}
+</style>
