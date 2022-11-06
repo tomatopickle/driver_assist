75592fa7dd563d6a7c5f8f5298f87ba96697c3f7
Abaan Shanid <abaanashanid@gmail.com>
2022-11-06 14:08:40 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h2>Driver Assist</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text fab>
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container style="display: grid; height: 100%; flex-flow: column">
+    <v-row>
+      <v-col class="mb-4">
+        <h1 class="font-weight-bold mt-2 ml-2 mb-3">Good Evening</h1>
+        <br />
+        <v-card elevation="2">
+          <v-card-title>Stats</v-card-title>
+          <v-card-text>Your driving statics show up here</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn elevated>View More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <router-link to="./Trip" style="margin-top: auto">
+      <v-btn block color="primary">Start Trip</v-btn>
+    </router-link>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
+
+  components: {},
 };
 </script>
