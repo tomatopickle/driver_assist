a4a5f6cf0eb14e89db450c04790750eae0a84de6
Abaan Shanid <abaanashanid@gmail.com>
2022-11-07 18:37:54 +0300

    Feat: Improved theming

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
+import { colors } from "vuetify/lib";
 import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({ theme: { dark: true } });
+export default new Vuetify({
+  theme: { dark: true, themes: { dark: { primary: colors.teal } } },
+});
